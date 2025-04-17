74ecd7c01e46d3fb505f87882f8ea7edf8f1d90e
John Arne Føreland <john.foreland@me.com>
2025-04-17 23:10:48 +0200

    added thesis pdf modal

---
+++ src/app/about/thesis-modal.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Dialog, Button, Column } from "@/once-ui/components";
+import React from "react";
+
+export default function ThesisModal({
+  name,
+  url,
+}: {
+  name: string;
+  url: string;
+}) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <>
+      <Button
+        variant="primary"
+        size="m"
+        onClick={() => setIsOpen(!isOpen)}
+        label="View Thesis"
+      ></Button>
+
+      <Dialog
+        fillWidth
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        title="Thesis"
+      >
+        <object
+          data={url}
+          title={name}
+          width="100%"
+          height="800"
+          style={{ border: "none" }}
+        ></object>
+      </Dialog>
+    </>
+  );
+}
