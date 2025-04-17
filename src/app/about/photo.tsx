70c1c812d2938cf3a44855d247d5798734c94472
John Arne Føreland <john.foreland@me.com>
2025-04-17 16:08:43 +0200

    moved photo to separate page

---
+++ src/app/about/photo.tsx
@@ -0,0 +1,45 @@
+import { Column, Flex, Tag } from "@/once-ui/components";
+import styles from "@/components/about/about.module.scss";
+import { person, about } from "@/app/resources/content";
+import SkyAvatar from "../../../components/sky-avatar/sky-avatar";
+
+export default function Photo() {
+  return (
+    <>
+      {about.avatar.display && (
+        <Column
+          className={styles.avatar}
+          minWidth="160"
+          paddingX="l"
+          paddingBottom="xl"
+          gap="m"
+          flex={3}
+          horizontal="center"
+        >
+          <Flex
+            gap="128"
+            direction="column"
+            mobileDirection="row"
+            vertical="center"
+            horizontal="center"
+            fillWidth={true}
+          >
+            <div style={{ width: "50px" }}></div>
+            <SkyAvatar></SkyAvatar>
+            <div style={{ width: "50px" }}></div>
+          </Flex>
+
+          {person.languages.length > 0 && (
+            <Flex wrap gap="8">
+              {person.languages.map((language, index) => (
+                <Tag key={index} size="l">
+                  {language}
+                </Tag>
+              ))}
+            </Flex>
+          )}
+        </Column>
+      )}
+    </>
+  );
+}
