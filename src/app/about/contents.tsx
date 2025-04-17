ad190abcc80d136045b7ff2cc843bb32b8358226
John Arne Føreland <john.foreland@me.com>
2025-04-17 16:06:31 +0200

    moved table of contents to separate file

---
+++ src/app/about/contents.tsx
@@ -0,0 +1,45 @@
+import { Column } from "@/once-ui/components";
+import TableOfContents from "@/components/about/TableOfContents";
+import { about } from "@/app/resources/content";
+
+export default function Contents() {
+  const structure = [
+    {
+      title: about.intro.title,
+      display: about.intro.display,
+      items: [],
+    },
+    {
+      title: about.work.title,
+      display: about.work.display,
+      items: about.work.experiences.map((experience) => experience.company),
+    },
+    {
+      title: about.studies.title,
+      display: about.studies.display,
+      items: about.studies.institutions.map((institution) => institution.name),
+    },
+    {
+      title: about.technical.title,
+      display: about.technical.display,
+      items: about.technical.skills.map((skill) => skill.title),
+    },
+  ];
+
+  return (
+    <>
+      {about.tableOfContent.display && (
+        <Column
+          left="0"
+          style={{ top: "50%", transform: "translateY(-50%)" }}
+          position="fixed"
+          paddingLeft="24"
+          gap="32"
+          hide="s"
+        >
+          <TableOfContents structure={structure} about={about} />
+        </Column>
+      )}
+    </>
+  );
+}
