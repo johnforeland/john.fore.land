eeba2faf6a9953e1d971b08b9cdeffa77cb2e498
John Arne Føreland <john.foreland@me.com>
2025-04-19 17:53:37 +0200

    re-added source code and linked in codes

--- src/once-ui/icons.ts
+++ src/once-ui/icons.ts
@@ -41,7 +41,9 @@ import {
 
 import { RiVisaLine } from "react-icons/ri";
 
-import { FaDiscord, FaGithub, FaGoogle } from "react-icons/fa6";
+import { GoRepoForked } from "react-icons/go";
+
+import { FaDiscord, FaGithub, FaLinkedin, FaGoogle } from "react-icons/fa6";
 
 import { LuChevronsLeftRight } from "react-icons/lu";
 
@@ -50,6 +52,7 @@ export const iconLibrary: Record<string, IconType> = {
   chevronDown: HiChevronDown,
   chevronRight: HiChevronRight,
   chevronLeft: HiChevronLeft,
+  sourceCode: GoRepoForked,
   chevronsLeftRight: LuChevronsLeftRight,
   refresh: HiOutlineArrowPath,
   check: HiCheck,
@@ -67,6 +70,7 @@ export const iconLibrary: Record<string, IconType> = {
   close: HiMiniXMark,
   openLink: HiOutlineLink,
   discord: FaDiscord,
+  linkedin: FaLinkedin,
   home: PiHouseDuotone,
   gallery: PiImageDuotone,
   google: FaGoogle,
