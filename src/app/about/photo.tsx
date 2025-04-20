617c02c8b967edcdb1889767a48b7e2427e8c1e8
John Arne Føreland <john.foreland@me.com>
2025-04-20 10:30:59 +0200

    added photo cmp to parallax page

--- src/app/about/photo.tsx
+++ src/app/about/photo.tsx
@@ -3,43 +3,29 @@ import styles from "@/components/about/about.module.scss";
 import { person, about } from "@/app/resources/content";
 import SkyAvatar from "@/components/about/sky-avatar/SkyAvatar";
 
-export default function Photo() {
+export default function Photo({ width = 24 }: { width?: number }) {
   return (
-    <>
-      {about.avatar.display && (
-        <Column
-          className={styles.avatar}
-          minWidth="160"
-          paddingX="l"
-          paddingBottom="xl"
-          gap="m"
-          flex={3}
+    about.avatar.display && (
+      <Column
+        className={styles.avatar}
+        width={width}
+        paddingX="l"
+        paddingBottom="xl"
+        gap="m"
+        flex={3}
+        horizontal="center"
+      >
+        <Flex
+          direction="column"
+          mobileDirection="row"
+          vertical="center"
           horizontal="center"
         >
-          <Flex
-            gap="128"
-            direction="column"
-            mobileDirection="row"
-            vertical="center"
-            horizontal="center"
-            fillWidth={true}
-          >
-            <div style={{ width: "50px" }}></div>
-            <SkyAvatar></SkyAvatar>
-            <div style={{ width: "50px" }}></div>
-          </Flex>
-
-          {person.languages.length > 0 && (
-            <Flex wrap gap="8">
-              {person.languages.map((language, index) => (
-                <Tag key={index} size="l">
-                  {language}
-                </Tag>
-              ))}
-            </Flex>
-          )}
-        </Column>
-      )}
-    </>
+          <div style={{ width: "50px" }}></div>
+          <SkyAvatar></SkyAvatar>
+          <div style={{ width: "50px" }}></div>
+        </Flex>
+      </Column>
+    )
   );
 }
