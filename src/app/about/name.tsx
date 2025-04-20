3d2f84e7fcdcbd2602eafd46bbf0f6e4c3b1aaae
John Arne Føreland <john.foreland@me.com>
2025-04-20 15:06:51 +0200

    moved titles and social buttons to separate cmp

--- src/app/about/name.tsx
+++ src/app/about/name.tsx
@@ -1,16 +1,8 @@
-import {
-  Column,
-  Flex,
-  Heading,
-  Text,
-  Icon,
-  IconButton,
-  Button,
-  LogoCloud,
-  InlineCode,
-} from "@/once-ui/components";
+import { Column, Heading } from "@/once-ui/components";
 import styles from "@/components/about/about.module.scss";
-import { person, about, social } from "@/app/resources/content";
+import { person, about } from "@/app/resources/content";
+import Title from "./parts/Title";
+import SocialButtons from "./parts/SocialButtons";
 
 export default function Name() {
   return (
@@ -25,74 +17,10 @@ export default function Name() {
         <Heading className={styles.textAlign} variant="display-strong-xl">
           {person.name}
         </Heading>
-        <LogoCloud
-          columns="1"
-          style={{
-            transform: "translate(140px, -45px) rotate(-20deg) scale(1.4)",
-          }}
-          mobileColumns="1"
-          rotationInterval={3000}
-          limit={1}
-          logos={[
-            {
-              iconSrc: "/images/about/titles/frontend.gif",
-              wordmark: false,
-            },
-            {
-              iconSrc: "/images/about/titles/architect.gif",
-              wordmark: false,
-            },
-            {
-              iconSrc: "/images/about/titles/devops.gif",
-              wordmark: false,
-            },
-            {
-              iconSrc: "/images/about/titles/backend.gif",
-              wordmark: false,
-            },
-            {
-              iconSrc: "/images/about/titles/integration.gif",
-              wordmark: false,
-            },
-          ]}
-        />
 
-        {social.length > 0 && (
-          <Flex
-            className={styles.blockAlign}
-            style={{ transform: "translateY(-60px)" }}
-            paddingBottom="8"
-            gap="8"
-            wrap
-            horizontal="center"
-            fitWidth
-          >
-            {social.map(
-              (item) =>
-                item.link && (
-                  <div key={item.name}>
-                    <Button
-                      className="s-flex-hide"
-                      key={item.name}
-                      href={item.link}
-                      prefixIcon={item.icon}
-                      label={item.name}
-                      size="s"
-                      variant="secondary"
-                    />
-                    <IconButton
-                      className="s-flex-show"
-                      size="l"
-                      key={`${item.name}-icon`}
-                      href={item.link}
-                      icon={item.icon}
-                      variant="secondary"
-                    />
-                  </div>
-                )
-            )}
-          </Flex>
-        )}
+        <Title />
+
+        <SocialButtons />
       </Column>
     </>
   );
