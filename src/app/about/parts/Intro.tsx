b70b634b8a0d7770eea3d95357f386b5ff996561
John Arne Føreland <john.foreland@me.com>
2025-04-21 16:29:51 +0200

    improved /about content placement

--- src/app/about/parts/Intro.tsx
+++ src/app/about/parts/Intro.tsx
@@ -1,28 +1,15 @@
 import { Column, Heading } from "@/once-ui/components";
-import styles from "@/components/about/about.module.scss";
-import { person, about } from "@/app/resources/content";
+import { about } from "@/app/resources/content";
 
-export default function Intro() {
+export default function Intro({ className }: { className?: string }) {
   return (
     about.intro.display && (
-      <>
-        <Heading
-          as="h2"
-          id={about.intro.title}
-          variant="display-strong-s"
-          marginBottom="m"
-        >
-          {about.intro.title}
-        </Heading>
-        <Column
-          textVariant="body-default-l"
-          fillWidth
-          gap="m"
-          marginBottom="xl"
-        >
+      <div className={className}>
+        <h2 className="text-4xl font-bold pb-10">{about.intro.title}</h2>
+        <div className="text-2xl mx-4 md:pr-6 text-gray-500">
           {about.intro.description}
-        </Column>
-      </>
+        </div>
+      </div>
     )
   );
 }
