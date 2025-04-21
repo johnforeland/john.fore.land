bf64f6f0c2a3ebea43ddb8a2f904734f3a8876de
John Arne Føreland <john.foreland@me.com>
2025-04-21 22:45:18 +0200

    changed once-ui → tailwind for multiple components

--- src/app/about/parts/Skills.tsx
+++ src/app/about/parts/Skills.tsx
@@ -1,55 +1,46 @@
-import { Column, Flex, Heading, Text, SmartImage } from "@/once-ui/components";
 import { about } from "@/app/resources/content";
+import { Flex, SmartImage } from "@/once-ui/components";
 
 export default function Skills({ className }: { className?: string }) {
   return (
-    about.technical.display && (
-      <div className={className}>
-        <Heading
-          as="h2"
-          id={about.technical.title}
-          variant="display-strong-s"
-          marginBottom="40"
-        >
-          {about.technical.title}
-        </Heading>
-        <Column fillWidth gap="l">
-          {about.technical.skills.map((skill, index) => (
-            <Column key={`${skill}-${index}`} fillWidth gap="4">
-              <Text variant="heading-strong-l">{skill.title}</Text>
-              <Text variant="body-default-m" onBackground="neutral-weak">
-                {skill.description}
-              </Text>
-              {skill.images && skill.images.length > 0 && (
-                <Flex fillWidth paddingTop="m" gap="12" wrap>
-                  {skill.images.map((image, index) => (
-                    <Flex
-                      key={index}
-                      border="neutral-medium"
+    <div className={className}>
+      <h2 className="text-3xl font-bold mb-12">{about.technical.title}</h2>
+
+      <div className="flex flex-col gap-6 ">
+        {/* loop skills */}
+        {about.technical.skills.map((skill, index) => (
+          <div key={`${skill}-${index}`}>
+            {/* title */}
+            <p className="text-xl font-semibold">{skill.title}</p>
+
+            {/* description */}
+            <p className="text-sm text-gray-500">{skill.description}</p>
+
+            {/* loop images */}
+            {skill.images && skill.images.length > 0 && (
+              <div className="flex flex-wrap w-full pt-6 gap-3">
+                {skill.images.map((image, index) => (
+                  <Flex
+                    key={index}
+                    border="neutral-medium"
+                    radius="m"
+                    minWidth={image.width}
+                    height={image.height}
+                  >
+                    <SmartImage
+                      enlarge
                       radius="m"
-                      //@ts-ignore
-                      minWidth={image.width}
-                      //@ts-ignore
-                      height={image.height}
-                    >
-                      <SmartImage
-                        enlarge
-                        radius="m"
-                        //@ts-ignore
-                        sizes={image.width.toString()}
-                        //@ts-ignore
-                        alt={image.alt}
-                        //@ts-ignore
-                        src={image.src}
-                      />
-                    </Flex>
-                  ))}
-                </Flex>
-              )}
-            </Column>
-          ))}
-        </Column>
+                      sizes={image.width.toString()}
+                      alt={image.alt}
+                      src={image.src}
+                    />
+                  </Flex>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
       </div>
-    )
+    </div>
   );
 }
--- src/app/about/parts/Studies.tsx
+++ src/app/about/parts/Studies.tsx
@@ -6,26 +6,26 @@ import ThesisModal from "./ThesisModal";
 export default function Studies({ className }: { className?: string }) {
   return (
     <div className={className}>
-      <h2 id={about.studies.title} className="text-4xl font-bold mb-4">
-        {about.studies.title}
-      </h2>
-      <div className="flex flex-col w-full gap-6 mb-10">
+      <h2 className="text-4xl font-bold mb-12">{about.studies.title}</h2>
+
+      <div className="flex flex-col gap-6 ">
+        {/* loop */}
         {about.studies.institutions.map((institution, index) => (
-          <div
-            key={`${institution.name}-${index}`}
-            className="flex flex-col w-full gap-1"
-          >
+          <div key={`${institution.name}-${index}`} className="flex flex-col">
+            {/* title */}
             <div className="flex w-full justify-between items-end mb-1">
               <p className="text-xl font-semibold">{institution.name}</p>
               <p className="text-sm text-gray-500">{institution.timeframe}</p>
             </div>
+
+            {/* description */}
             <p className="text-sm text-gray-500">{institution.description}</p>
-            {institution.thesis && (
-              <ThesisModal
-                name={institution.thesis}
-                url={institution.thesisUrl}
-              />
-            )}
+
+            {/* thesis modal button */}
+            <ThesisModal
+              name={institution.thesis}
+              url={institution.thesisUrl}
+            />
           </div>
         ))}
       </div>
