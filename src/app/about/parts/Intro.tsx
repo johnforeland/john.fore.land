07e8a1ec056950687939de88b137c37bd936fb12
John Arne Føreland <john.foreland@me.com>
2025-04-21 19:50:11 +0200

    added parallax effect on /about title

--- src/app/about/parts/Intro.tsx
+++ src/app/about/parts/Intro.tsx
@@ -5,7 +5,6 @@ export default function Intro({ className }: { className?: string }) {
   return (
     about.intro.display && (
       <div className={className}>
-        <h2 className="text-4xl font-bold pb-10">{about.intro.title}</h2>
         <div className="text-2xl mx-4 md:pr-6 text-gray-500">
           {about.intro.description}
         </div>
