99f373ea637426a5af841bf679fd033757aa3be6
John Arne Føreland <john.foreland@me.com>
2025-04-21 16:15:38 +0200

    added className param to about components

--- src/app/about/parts/Skills.tsx
+++ src/app/about/parts/Skills.tsx
@@ -1,57 +1,55 @@
 import { Column, Flex, Heading, Text, SmartImage } from "@/once-ui/components";
 import { about } from "@/app/resources/content";
 
-export default function Skills() {
+export default function Skills({ className }: { className?: string }) {
   return (
-    <>
-      {about.technical.display && (
-        <>
-          <Heading
-            as="h2"
-            id={about.technical.title}
-            variant="display-strong-s"
-            marginBottom="40"
-          >
-            {about.technical.title}
-          </Heading>
-          <Column fillWidth gap="l">
-            {about.technical.skills.map((skill, index) => (
-              <Column key={`${skill}-${index}`} fillWidth gap="4">
-                <Text variant="heading-strong-l">{skill.title}</Text>
-                <Text variant="body-default-m" onBackground="neutral-weak">
-                  {skill.description}
-                </Text>
-                {skill.images && skill.images.length > 0 && (
-                  <Flex fillWidth paddingTop="m" gap="12" wrap>
-                    {skill.images.map((image, index) => (
-                      <Flex
-                        key={index}
-                        border="neutral-medium"
+    about.technical.display && (
+      <div className={className}>
+        <Heading
+          as="h2"
+          id={about.technical.title}
+          variant="display-strong-s"
+          marginBottom="40"
+        >
+          {about.technical.title}
+        </Heading>
+        <Column fillWidth gap="l">
+          {about.technical.skills.map((skill, index) => (
+            <Column key={`${skill}-${index}`} fillWidth gap="4">
+              <Text variant="heading-strong-l">{skill.title}</Text>
+              <Text variant="body-default-m" onBackground="neutral-weak">
+                {skill.description}
+              </Text>
+              {skill.images && skill.images.length > 0 && (
+                <Flex fillWidth paddingTop="m" gap="12" wrap>
+                  {skill.images.map((image, index) => (
+                    <Flex
+                      key={index}
+                      border="neutral-medium"
+                      radius="m"
+                      //@ts-ignore
+                      minWidth={image.width}
+                      //@ts-ignore
+                      height={image.height}
+                    >
+                      <SmartImage
+                        enlarge
                         radius="m"
                         //@ts-ignore
-                        minWidth={image.width}
+                        sizes={image.width.toString()}
                         //@ts-ignore
-                        height={image.height}
-                      >
-                        <SmartImage
-                          enlarge
-                          radius="m"
-                          //@ts-ignore
-                          sizes={image.width.toString()}
-                          //@ts-ignore
-                          alt={image.alt}
-                          //@ts-ignore
-                          src={image.src}
-                        />
-                      </Flex>
-                    ))}
-                  </Flex>
-                )}
-              </Column>
-            ))}
-          </Column>
-        </>
-      )}
-    </>
+                        alt={image.alt}
+                        //@ts-ignore
+                        src={image.src}
+                      />
+                    </Flex>
+                  ))}
+                </Flex>
+              )}
+            </Column>
+          ))}
+        </Column>
+      </div>
+    )
   );
 }
--- src/app/about/parts/Work.tsx
+++ src/app/about/parts/Work.tsx
@@ -1,92 +1,87 @@
 import { Column, Flex, Heading, Text, SmartImage } from "@/once-ui/components";
 import { about } from "@/app/resources/content";
 
-export default function Work() {
+export default function Work({ className }: { className?: string }) {
   return (
-    <>
-      {about.work.display && (
-        <>
-          <Heading
-            as="h2"
-            id={about.work.title}
-            variant="display-strong-s"
-            marginBottom="m"
-          >
-            {about.work.title}
-          </Heading>
-          <Column fillWidth gap="l" marginBottom="40">
-            {about.work.experiences.map((experience, index) => (
-              <Column
-                key={`${experience.company}-${experience.role}-${index}`}
+    about.work.display && (
+      <div className={className}>
+        <Heading
+          as="h2"
+          id={about.work.title}
+          variant="display-strong-s"
+          marginBottom="m"
+        >
+          {about.work.title}
+        </Heading>
+        <Column fillWidth gap="l" marginBottom="40">
+          {about.work.experiences.map((experience, index) => (
+            <Column
+              key={`${experience.company}-${experience.role}-${index}`}
+              fillWidth
+            >
+              <Flex
                 fillWidth
+                horizontal="space-between"
+                vertical="end"
+                marginBottom="4"
               >
-                <Flex
-                  fillWidth
-                  horizontal="space-between"
-                  vertical="end"
-                  marginBottom="4"
-                >
-                  <Text id={experience.company} variant="heading-strong-l">
-                    {experience.company}
-                  </Text>
-                  <Text
-                    variant="heading-default-xs"
-                    onBackground="neutral-weak"
-                  >
-                    {experience.timeframe}
-                  </Text>
-                </Flex>
-                <Text
-                  variant="body-default-s"
-                  onBackground="brand-weak"
-                  marginBottom="m"
-                >
-                  {experience.role}
+                <Text id={experience.company} variant="heading-strong-l">
+                  {experience.company}
                 </Text>
-                <Column as="ul" gap="16">
-                  {experience.achievements.map(
-                    (achievement: JSX.Element, index: number) => (
-                      <Text
-                        as="li"
-                        variant="body-default-m"
-                        key={`${experience.company}-${index}`}
-                      >
-                        {achievement}
-                      </Text>
-                    )
-                  )}
-                </Column>
-                {experience.images.length > 0 && (
-                  <Flex fillWidth paddingTop="m" paddingLeft="40" wrap>
-                    {experience.images.map((image, index) => (
-                      <Flex
-                        key={index}
-                        border="neutral-medium"
-                        radius="m"
-                        //@ts-ignore
-                        minWidth={image.width}
-                        //@ts-ignore
-                        height={image.height}
-                      >
-                        <SmartImage
-                          enlarge
-                          radius="m"
-                          //@ts-ignore
-                          sizes={image.width.toString()}
-                          //@ts-ignore
-                          alt={image.alt}
-                          //@ts-ignore
-                          src={image.src}
-                        />
-                      </Flex>
-                    ))}
-                  </Flex>
+                <Text variant="heading-default-xs" onBackground="neutral-weak">
+                  {experience.timeframe}
+                </Text>
+              </Flex>
+              <Text
+                variant="body-default-s"
+                onBackground="brand-weak"
+                marginBottom="m"
+              >
+                {experience.role}
+              </Text>
+              <Column as="ul" gap="16">
+                {experience.achievements.map(
+                  (achievement: JSX.Element, index: number) => (
+                    <Text
+                      as="li"
+                      variant="body-default-m"
+                      key={`${experience.company}-${index}`}
+                    >
+                      {achievement}
+                    </Text>
+                  )
                 )}
               </Column>
-            ))}
-          </Column>
-        </>
-      )}
-    </>
+              {experience.images.length > 0 && (
+                <Flex fillWidth paddingTop="m" paddingLeft="40" wrap>
+                  {experience.images.map((image, index) => (
+                    <Flex
+                      key={index}
+                      border="neutral-medium"
+                      radius="m"
+                      //@ts-ignore
+                      minWidth={image.width}
+                      //@ts-ignore
+                      height={image.height}
+                    >
+                      <SmartImage
+                        enlarge
+                        radius="m"
+                        //@ts-ignore
+                        sizes={image.width.toString()}
+                        //@ts-ignore
+                        alt={image.alt}
+                        //@ts-ignore
+                        src={image.src}
+                      />
+                    </Flex>
+                  ))}
+                </Flex>
+              )}
+            </Column>
+          ))}
+        </Column>
+      </div>
+    )
   );
 }
