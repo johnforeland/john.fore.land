998013e49ebfe905a4f5891f7d16f22b7f423a44
John Arne Føreland <john.foreland@me.com>
2025-04-22 19:09:47 +0200

    added missing jsx import

--- src/components/HeadingLink.tsx
+++ src/components/HeadingLink.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React from "react";
-import { Heading, Flex, IconButton, useToast } from "@/once-ui/components";
+import { Flex, Heading, IconButton, useToast } from "@/once-ui/components";
+import React, { JSX } from "react";
 
 import styles from "@/components/HeadingLink.module.scss";
 
@@ -12,7 +12,12 @@ interface HeadingLinkProps {
   style?: React.CSSProperties;
 }
 
-export const HeadingLink: React.FC<HeadingLinkProps> = ({ id, level, children, style }) => {
+export const HeadingLink: React.FC<HeadingLinkProps> = ({
+  id,
+  level,
+  children,
+  style,
+}) => {
   const { addToast } = useToast();
 
   const copyURL = (id: string): void => {
@@ -29,7 +34,7 @@ export const HeadingLink: React.FC<HeadingLinkProps> = ({ id, level, children, s
           variant: "danger",
           message: "Failed to copy link.",
         });
-      },
+      }
     );
   };
 
