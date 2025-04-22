ab4c45b4cc826d56ea8d16af1ee5735bbc684979
John Arne Føreland <john.foreland@me.com>
2025-04-22 17:46:26 +0200

    changed value to const

--- src/components/work/Projects.tsx
+++ src/components/work/Projects.tsx
@@ -7,7 +7,7 @@ interface ProjectsProps {
 }
 
 export function Projects({ range }: ProjectsProps) {
-  let allProjects = getPosts(["src", "app", "work", "projects"]);
+  const allProjects = getPosts(["src", "app", "work", "projects"]);
 
   const sortedProjects = allProjects.sort((a, b) => {
     return (
