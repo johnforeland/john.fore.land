741655199c34fbeeb24b6650ebbb49c71fcf7317
John Arne Føreland <john.foreland@me.com>
2025-05-20 00:01:36 +0200

    added /stats page

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,11 +1,17 @@
 import { stats } from "@/resources/content";
 
 import { generateMetadataObject } from "@/utils/utils";
+import { Terminal } from "./terminal";
 
 export async function generateMetadata() {
   return generateMetadataObject(stats.title, stats.description, "stats");
 }
 
 export default function Stats() {
-  return <div className="max-w-md mx-auto"></div>;
+  console.log(process.title);
+  return (
+    <div className="w-full ">
+      <Terminal nodeVersion={process.version} nextVersion={process.title} />
+    </div>
+  );
 }
