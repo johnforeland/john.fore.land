30f46c782307fec195ff0c348686951050d1ac2c
John Arne Føreland <john.foreland@me.com>
2025-05-23 18:10:35 +0200

    Renamed Header → Navigation and made it always stay at bottom of screen

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,5 +1,5 @@
 export { default as GenerateScript } from "./GenerateScript";
-export { Header } from "./Header";
+export { Navigation } from "./Navigation";
 export { default as Padding } from "./Padding";
 export { default as ParallaxProvider } from "./Parallax";
 export { default as Root } from "./Root";
