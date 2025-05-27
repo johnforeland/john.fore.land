37e00f380de5b8fb0ecdbc8bdd9593009814cf4f
John Arne Føreland <john.foreland@me.com>
2025-05-27 23:58:07 +0200

    added new animating background

--- src/components/backgrounds/AnimationBackground.tsx
+++ src/components/backgrounds/AnimationBackground.tsx
@@ -1,3 +1,17 @@
+import { BackgroundGradientAnimation } from "./background-gradient-animation";
+
+import "./AnimationBackground.css";
+
 export function AnimationBackground() {
-  return <p>1</p>;
+  return (
+    <div className="fixed inset-0 -z-10 w-full h-full">
+      <BackgroundGradientAnimation
+        gradientBackgroundStart="rgb(0,0,0,0)"
+        gradientBackgroundEnd="rgb(0,0,0,0)"
+        size="20%"
+        interactive={false}
+        blendingValue="hard-light"
+      />
+    </div>
+  );
 }
