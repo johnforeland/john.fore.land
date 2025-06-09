784853e8d13ef9fb46d63d1f834380e3d56bbcc1
John Arne Føreland <john.foreland@me.com>
2025-06-09 21:49:18 +0200

    made functions for easier jsx compiling

--- src/components/github/GitHubCard.tsx
+++ src/components/github/GitHubCard.tsx
@@ -7,47 +7,60 @@ import { fetchGitHubRepo } from "./GitHubRepoFetcher";
 export async function GitHubCard({ repo }: GitHubCardProps) {
   const repoInfo = await fetchGitHubRepo(repo.username, repo.repo);
 
+  function getHeader(): import("react").ReactNode {
+    return repo.image ? getImage() : getCode();
+  }
+
+  function getImage(): import("react").ReactNode {
+    return (
+      <Image
+        src={repo.image}
+        alt={`${repo.username}/${repo.repo}`}
+        width={384}
+        height={216}
+        style={{ width: "100%", height: "auto" }}
+      />
+    );
+  }
+
+  function getCode(): import("react").ReactNode {
+    return (
+      <div className="mockup-code w-full">
+        <SyntaxHighlighter
+          language="yml"
+          style={dark}
+          customStyle={{ background: "none" }}
+        >
+          {repo.code}
+        </SyntaxHighlighter>
+      </div>
+    );
+  }
+
+  function getTags(): import("react").ReactNode {
+    if (!repo.tags || repo.tags.length == 0) {
+      return (
+        repoInfo.language && (
+          <div className="badge badge-outline">{repoInfo.language}</div>
+        )
+      );
+    }
+
+    return repo.tags.map((tag: string) => (
+      <div key={tag} className="badge badge-outline">
+        {tag}
+      </div>
+    ));
+  }
+
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
-      {repo.image && (
-        <figure>
-          <Image
-            src={repo.image}
-            alt={`${repo.username}/${repo.repo}`}
-            width={384}
-            height={216}
-            style={{ width: "100%", height: "auto" }}
-          />
-        </figure>
-      )}
-      {repo.code && (
-        <figure>
-          <div className="mockup-code w-full">
-            <SyntaxHighlighter
-              language="yml"
-              style={dark}
-              customStyle={{ background: "none" }}
-            >
-              {repo.code}
-            </SyntaxHighlighter>
-          </div>
-        </figure>
-      )}
+      <figure>{getHeader()}</figure>
       <div className="card-body">
         <h2 className="card-title">{repoInfo.name}</h2>
         <h3>{repoInfo.organization}</h3>
         <p>{repoInfo.description || "No description available."}</p>
-        <div className="card-actions justify-end">
-          {repo.tags && repo.tags.length > 0
-            ? repo.tags.map((tag: string) => (
-                <div key={tag} className="badge badge-outline">
-                  {tag}
-                </div>
-              ))
-            : repoInfo.language && (
-                <div className="badge badge-outline">{repoInfo.language}</div>
-              )}
-        </div>
+        <div className="card-actions justify-end">{getTags()}</div>
       </div>
     </div>
   );
