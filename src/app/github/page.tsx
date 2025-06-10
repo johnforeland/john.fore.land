c487d82cbe8a31a15ed31a92a1804f585d438fb2
John Arne Føreland <john.foreland@me.com>
2025-06-10 19:09:02 +0200

    render items with smallest possible height

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -3,9 +3,11 @@ import { github } from "@/resources/content";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-6xl mx-auto pb-22 md:pd-0">
+    <div className="columns-1 md:columns-2 gap-6 max-w-6xl mx-auto pb-22 md:pd-0">
       {github.repos.map((repo) => (
-        <GitHubCard key={repo.repo} repo={repo} />
+        <div key={repo.repo} className="mb-6 break-inside-avoid">
+          <GitHubCard repo={repo} />
+        </div>
       ))}
     </div>
   );
