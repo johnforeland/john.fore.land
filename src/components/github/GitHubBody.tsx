bbfbb5bcb209b4ac54d198fcbfb977d4e65f7f48
John Arne Føreland <john.foreland@me.com>
2025-06-10 21:09:58 +0200

    improved github card body

--- src/components/github/GitHubBody.tsx
+++ src/components/github/GitHubBody.tsx
@@ -20,16 +20,36 @@ export async function GitHubBody({ repo }: GitHubCardProps) {
     );
   }
 
+  function getOwner(): import("react").ReactNode {
+    return (
+      <a
+        href={"https://github.com/" + repoInfo.organization}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {repoInfo.organization}
+      </a>
+    );
+  }
+
   return (
     <div className="card-body">
-      <h2 className="card-title">{repoInfo.name}</h2>
-      <h3>{repoInfo.organization}</h3>
+      <div className="grid grid-cols-2  mb-4">
+        <div className="flex items-center">
+          <h2 className="card-title">{repoInfo.name}</h2>
+        </div>
+        <div className="flex justify-end items-center">
+          <div className="flex gap-2 flex-wrap">{getTags()}</div>
+        </div>
+        <h3>{getOwner()}</h3>
+      </div>
+
       <h4>Commits: {commitAmount}</h4>
       {repoInfo.latestRelease && (
         <h4>Latest Release: {repoInfo.latestRelease.name}</h4>
       )}
       <p>{repoInfo.description || "No description available."}</p>
-      <div className="card-actions justify-end">{getTags()}</div>
+      <div className="card-actions justify-end"></div>
     </div>
   );
 }
