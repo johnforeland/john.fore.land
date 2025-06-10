7ed3eac9af9ea126b925ee85a7a19dab2061c63b
John Arne Føreland <john.foreland@me.com>
2025-06-10 20:50:36 +0200

    re-added column sorting

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -3,7 +3,7 @@ import { github } from "@/resources/content";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-6xl mx-auto px-4 md:px-0 pb-6 md:pb-0 overflow-x-hidden">
+    <div className="columns-1 md:columns-2 gap-6 max-w-6xl mx-auto px-4 md:px-0 pb-6 md:pb-0 overflow-x-hidden">
       {github.repos.map((repo) => (
         <div key={repo.repo} className="mb-6 break-inside-avoid">
           <GitHubCard repo={repo} />
